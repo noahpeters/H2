00e7b8daf86ceadcaa58f7be22879b720fcb4057
Noah Peters <noahpeters@gmail.com>
2026-09-10 23:02:28 -0700

    Fix core sitemap coverage

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -5,9 +5,13 @@ export async function loader({
   request,
   context: {storefront},
 }: Route.LoaderArgs) {
+  const canonicalUrl = new URL(request.url);
+  canonicalUrl.protocol = 'https:';
+  canonicalUrl.host = 'from-trees.com';
+
   const response = await getSitemapIndex({
     storefront,
-    request,
+    request: new Request(canonicalUrl, request),
     types: ['products', 'collections', 'pages', 'articles'],
     customChildSitemaps: ['/sitemap.custom.xml'],
   });
